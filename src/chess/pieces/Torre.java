3c9ce5fe077ed6d5e0c5d80e89ee0514afee6946
Diego-Sant <diego_santana@unigranrio.br>
2022-11-07 20:31:12 -0300

    Torre - Rei

---
+++ src/chess/pieces/Torre.java
@@ -0,0 +1,18 @@
+package chess.pieces;
+
+import board.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Torre extends ChessPiece {
+
+	public Torre(Board board, Color color) {
+		super(board, color);
+	}
+
+	@Override
+	public String toString() {
+		return "♦";
+	}
+	
+}
