696ee69e960ee6ce4aaf4a1682e44e5b39e12378
Diego-Sant <diego_santana@unigranrio.br>
2022-11-07 21:07:40 -0300

    ChessException - ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import board.Board;
-import board.Position;
 import chess.pieces.Rei;
 import chess.pieces.Torre;
 
@@ -24,9 +23,14 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Torre(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new Rei(board, Color.BLACK), new Position(0, 4));
+		placeNewPiece('a', 1, new Torre(board, Color.WHITE));
+		placeNewPiece('e', 8, new Rei(board, Color.WHITE));
+		placeNewPiece('e', 1, new Rei(board, Color.BLACK));
 	}
 	
 }
