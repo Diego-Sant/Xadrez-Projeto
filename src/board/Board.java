dbb00cd968e99ee5b110b268e986e158fe03e366
Diego-Sant <diego_santana@unigranrio.br>
2022-11-09 16:14:12 -0300

    Moving Pieces

--- src/board/Board.java
+++ src/board/Board.java
@@ -31,6 +31,9 @@ public class Board {
 		this.pieces = pieces;
 	}
 	
+	
+	
+	
 	public Piece piece(int row, int column) {
 		if (!positionExists(row, column)) {
 			throw new BoardException("Essa posição não está no tabuleiro!");
@@ -38,12 +41,18 @@ public class Board {
 		return pieces[row][column];
 	}
 	
+	
+	
+	
 	public Piece piece(Position position) {
 		if (!positionExists(position)) {
 			throw new BoardException("Essa posição não está no tabuleiro!");
 		}
 		return pieces[position.getRow()][position.getColumn()];
 	}
+	
+	
+	
 	
 	public void placePiece(Piece piece, Position position) {
 		if (thereIsAPiece(position)) {
@@ -53,13 +62,38 @@ public class Board {
 		piece.position = position;
 	}
 	
+	
+	
+	
+	public Piece removePiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Essa posição não está no tabuleiro!");
+		}
+		if (piece(position) == null) {
+			return null;
+		}
+		Piece aux = piece(position);
+		aux.position = null;
+		pieces[position.getRow()][position.getColumn()] = null;
+		return aux;
+	}
+	
+	
+	
+	
 	private boolean positionExists(int row, int column) {
 		return row >= 0 && row < rows && column >= 0 && column < columns;
 	}
 	
+	
+	
+	
 	public boolean positionExists(Position position) {
 		return positionExists(position.getRow(), position.getColumn());
 	}
+	
+	
+	
 	
 	public boolean thereIsAPiece(Position position) {
 		if (!positionExists(position)) {
