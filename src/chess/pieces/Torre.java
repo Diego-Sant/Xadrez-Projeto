db2c24c476cc8892eca33b43d540da31a45fea75
Diego-Sant <diego_santana@unigranrio.br>
2022-11-09 18:03:02 -0300

    Possible Moves

--- src/chess/pieces/Torre.java
+++ src/chess/pieces/Torre.java
@@ -14,5 +14,11 @@ public class Torre extends ChessPiece {
 	public String toString() {
 		return "♦";
 	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		return mat;
+	}
 	
 }
